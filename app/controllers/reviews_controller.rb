894c3a2113dd6b4beea13f2ef9002120f9cb48b2
Dan Jacob and Katie Borel <dan+katie@>
2017-06-28 14:22:42 -0700

    add scope to display top ten most recently created users

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,22 @@
+class ReviewsController < ApplicationController
+
+  def new
+    @review = Review.new
+    @user = User.find(params[:user_id])
+
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to  user_episode(@user.episode)
+    else
+      render :new
+    end
+  end
+
+private
+def review_params
+  params.require(:review).permit(:content,:review, :user_id, :episode_id)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  scope :ten_most_recent, -> { order(created_at: :desc).limit(5)}
   validates :name, :presence => true
   has_many :reviews
 end
